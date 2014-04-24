a59c90ecfaed71105b5c2b9ca29ba9d4f36da8e8
Contat Paul <contat.paul@gmail.com>
2014-04-24 18:58:57 +0200

    Création de la fonctionalité ICS exportable + nettoyage dossier
    Le script ICS est fonctionel il faudra apporter quelque modification mineur suivant l'implementation
    IMPORTANT pour les Api de connexion :
    -Penssez a changer le base_url de conf si on change de dossier
    -Idem pour les URI sur les site de Google/Facebook/LinkedIn
    - Curl sur php 5.4.X sur wamp bug : télécharger le corectif sur http://www.anindya.com/php-5-4-3-and-php-5-3-13-x64-64-bit-for-windows/

--- feature/map/script.js
+++ feature/map/script.js
@@ -94,13 +94,13 @@ function manageChoices(input)
 			{
 				choices = input.parentNode.parentNode;
 				choices.removeChild(input.parentNode);
-				// var numero = input.parentNode.childNodes[1].getAttribute("id");
+				var numero = input.parentNode.children[1].getAttribute("id");
 				
-				var numero = input.parentNode.textContent.replace("Choix ","");
+				/*var numero = input.parentNode.textContent.replace("Choix ","");
 				numero = numero.replace("*","");
 				numero = numero.replace("x","");
 				numero = numero.replace("V","");
-				numero = parseInt(numero);		
+				numero = parseInt(numero);	*/	
 				alert(numero);
 				deleteMarkers(numero);
 				// version map
