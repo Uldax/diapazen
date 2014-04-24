b447bdbbea9d2c60c4fa6ed54fbd7426ce5a95b0
Alexis Brisset <alex2.brisset@gmail.com>
2014-04-24 15:48:25 +0200

    Map, fonctionnement de la suppression

--- feature/map/index.php
+++ feature/map/index.php
@@ -31,125 +31,115 @@
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCNOpHpuGzB9PguOdh3naoMxPzBhOonyo4&v=3.exp&sensor=false"></script>
     <!-- Chargement de place API pour l'autocomplétion de l'input" -->
     <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?libraries=places&amp;sensor=false"></script>
+    <script type="text/javascript" src="script.js"></script>
     <script>
 var geocoder;
 var map;
 var markers=[];
 var posmarkers = [];
+var boolMarker = false; // booleen qui définit si un marqueur est déjà placé à la même position (true si déjà placé)
 var zonemarqueurs = new google.maps.LatLngBounds();
 
 function initialize() {
   geocoder = new google.maps.Geocoder();
 
   //faire la geoloc à ++++++ce niveau (Centre sur la france par defaut)
-  var latlng = new google.maps.LatLng(46.3333,  2.6);
-  var mapOptions = {
+  latlng = new google.maps.LatLng(46.3333,  2.6);
+   mapOptions = {
     zoom: 5,
     center: latlng
   }
 
    //Instance de l'objet autocomplete avec comme restriction géographique la France
-  var options = {
+  options = {
      types: ['geocode'],
-     componentRestrictions: {country: 'fr'} // restriction vis-à-vis des propositions de la recherche des villes
+     // componentRestrictions: {country: 'fr'} // restriction vis-à-vis des propositions de la recherche des villes
   };
-  var input = document.getElementById('address');
-  autocomplete = new google.maps.places.Autocomplete(input, options);
 
+//function de création de input autovomplete
+  var input1 = document.getElementById('choix1');
+  var input2 = document.getElementById('choix2');
+  autocomplete1 = new google.maps.places.Autocomplete(input1, options);
+  autocomplete2 = new google.maps.places.Autocomplete(input2, options);
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-  google.maps.event.addListener(autocomplete, 'place_changed', function() {
-   var place = autocomplete.getPlace();
-   if (place.geometry.viewport) {
-      geomap.fitBounds(place.geometry.viewport);
-   } else {
-      geomap.setCenter(place.geometry.location);
-      geomap.setZoom(17);
-   }
-});
-
 }
 
 
 // fonction qui allie auto-complétion et ajout des marqueurs sur la carte
-function codeAddress() {
-
-  var address = document.getElementById('address').value;
-  geocoder.geocode( { 'address': address}, function(resultat, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-     addMarker(resultat);
-     map.fitBounds(zonemarqueurs);
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-  });
-}
 
 
 // Fonction qui permet d'ajouter les marqueurs sur la carte en fonction de la zone de recherche avec l'auto-complétion
-function addMarker(resultat){
-  var marker = new google.maps.Marker({
-    position: resultat[0].geometry.location,
-    map: map
-  });
-  markers.push(marker);
-  posmarkers.push(marker.getPosition());
-  zonemarqueurs.extend(marker.getPosition());
-}
 
 
 
 // Sets the map on all markers in the array.
-function setAllMap(map) {
-  for (var i = 0; i < markers.length; i++) {
-    markers[i].setMap(map);
-  }
-}
+// function setAllMap(map) {
+//   for (var i = 0; i < markers.length; i++) {
+//     markers[i].setMap(map);
+//   }
+// }
 
-// Removes the markers from the map, but keeps them in the array.
-function clearMarkers() {
-  setAllMap(null);
-}
+// // Removes the markers from the map, but keeps them in the array.
+// function clearMarkers() {
+//   setAllMap(null);
+// }
 
-// Deletes all markers in the array by removing references to them.
-function deleteMarkers() {
-  clearMarkers();
-  markers = [];
-  posmarkers = [];
-  zonemarqueurs = new google.maps.LatLngBounds();
-  initialize();
-}
+// // Deletes all markers in the array by removing references to them.
+// function deleteMarkers() {
+//   clearMarkers();
+//   markers = [];
+//   posmarkers = [];
+//   zonemarqueurs = new google.maps.LatLngBounds();
+//   initialize();
+// }
 
-//Deletes the last entered marker in the array
-function deleteLastMarker(){
-  var j = markers.length;
-  if(j>1){
-    markers[j-1].setMap(null);
-    markers.pop();
-    posmarkers.pop();
-    var zonemarqueurs = new google.maps.LatLngBounds();
-    for (var i =0; i <markers.length;i++){
-      zonemarqueurs.extend(markers[i].getPosition());
-    }
-    map.fitBounds(zonemarqueurs);
-  }
-  else{
-    deleteMarkers();
-  }
-}
+// //Deletes the last entered marker in the array
+// function deleteLastMarker(){
+//   var j = markers.length;
+//   if(j>1){
+//     markers[j-1].setMap(null);
+//     markers.pop();
+//     posmarkers.pop();
+//     var zonemarqueurs = new google.maps.LatLngBounds();
+//     for (var i =0; i <markers.length;i++){
+//       zonemarqueurs.extend(markers[i].getPosition());
+//     }
+//     map.fitBounds(zonemarqueurs);
+//   }
+//   else{
+//     deleteMarkers();
+//   }
+// }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+//google.maps.event.addDomListener(window, 'load', initialize);
 
     </script>
 
   </head>
   <body>
-    <div id="panel">
+  <body onload="initialize()">
+  <!--   <div id="panel">
       <input id="address" type="textbox" size="60" value="">
       <input type="button" value="Ajouter" onclick="codeAddress()">
       <input type="button" value="Supprimer tous les marqueurs" onclick="deleteMarkers()">
       <input type="button" value="Supprimer le dernier marqueur" onclick="deleteLastMarker()">
-    </div>
+    </div> -->
+
+    <div id="choices">
+      <div id="choice">
+        <label for="choix1" class="text lbl_choice">Choix 1<span class="asterisc"> *</span></label>
+          <input class="text_edit input_choice" id="choix1" type="text" name="choices[]" value="" />
+          <a class="grey_button" title="Supprimer" type="button" onclick="manageChoices(this);">x</a>
+          <input type="button" value="V" class="valid" onClick="codeAddress(choix1);">
+       </div>
+       <div class="choice">
+          <label for="choix2" class="text lbl_choice">Choix 2<span class="asterisc"> *</span></label>
+          <input class="text_edit input_choice" id="choix2" type="text" name="choices[]" value="" />
+          <a class="grey_button" title="Supprimer" type="button" onclick="manageChoices(this);">x</a>
+          <input type="button" value="V" class="valid" onClick="codeAddress(choix2);">
+        </div>
+      </div>      
+      <a class="orange_button" title="Ajouter un champ" id="add_choice_button" type="button" onclick="manageChoices(this);">+</a>
     <div id="map-canvas"></div>
   </body>
 </html>
---
+++ feature/map/script.js
@@ -0,0 +1,248 @@
+function datepickerLoader() {
+
+	$('#datepicker').datepicker({
+		dateFormat : 'yy-mm-dd',
+		minDate : 0,
+		regional : 'fr'
+	});
+
+
+/* French initialisation for the jQuery UI date picker plugin. */
+/* Written by Keith Wood (kbwood{at}iinet.com.au),
+              Stéphane Nahmani (sholby@sholby.net),
+              Stéphane Raimbault <stephane.raimbault@gmail.com> */
+
+	$.datepicker.regional['fr'] = {
+		closeText: 'Fermer',
+		prevText: 'Précédent',
+		nextText: 'Suivant',
+		currentText: 'Aujourd\'hui',
+		monthNames: ['Janvier','Février','Mars','Avril','Mai','Juin',
+		'Juillet','Aout','Septembre','Octobre','Novembre','Décembre'],
+		monthNamesShort: ['Janv.','Fév.','Mars','Avril','Mai','Juin',
+		'Juil.','Août','Sept.','Oct.','Nov.','Déc.'],
+		dayNames: ['Dimanche','Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi'],
+		dayNamesShort: ['Dim.','Lun.','Mar.','Mer.','Jeu.','Ven.','Sam.'],
+		dayNamesMin: ['D','L','M','M','J','V','S'],
+		weekHeader: 'Sem.',
+		dateFormat: 'dd/mm/yy',
+		firstDay: 1,
+		isRTL: false,
+		showMonthAfterYear: false,
+		yearSuffix: ''};
+	$.datepicker.setDefaults($.datepicker.regional['fr']);
+}
+
+/*
+ *Fonction d'ajout ou suppression de choix lors 
+ *de la création d'un sondage
+ */
+function manageChoices(input)
+{
+
+
+	switch(input.className)
+	{
+
+		case 'orange_button':			
+
+			// Ajout d'un champ de choix
+			
+			choices = document.getElementById("choices");
+
+			choice = document.createElement("div");
+
+			//Créaion de l'input
+			var input=document.createElement("input");
+			input.setAttribute("id","");
+			input.className = "text_edit input_choice";
+			input.setAttribute("type","text");
+			input.setAttribute("name","choices[]");
+			
+			var label = document.createElement("label");
+			label.setAttribute("for","");
+			label.className = "text lbl_choice";
+
+			var a = document.createElement("a");
+			a.className = "grey_button"
+			a.setAttribute("title","Supprimer");
+			a.setAttribute("type","button");
+			a.setAttribute("onclick","manageChoices(this);");
+			a.innerText=" x ";
+
+			//Création du bouton
+			var button = document.createElement("button");
+			button.innerText = "V";
+			button.className ="valid";
+
+			choice.className = 'choice';
+			choice.appendChild(label);
+			choice.appendChild(input);
+			choice.appendChild(a);
+			choice.appendChild(button);
+
+			choices.appendChild(choice);
+			createAutocompletion(input);
+			
+		break;
+
+		case 'grey_button':
+
+			// Suppression du champ de choix
+			
+			if(document.getElementsByName("choices[]").length > 2)
+			{
+				choices = input.parentNode.parentNode;
+				choices.removeChild(input.parentNode);
+				//seconde méthode
+				// var numero = input.parentNode.childNodes[1].getAttribute("id");
+				
+				var numero = input.parentNode.textContent.replace("Choix ","");
+				numero = numero.replace("*","");
+				numero = numero.replace("x","");
+				numero = numero.replace("V","");
+				numero = parseInt(numero);		
+				alert(numero);
+				deleteMarkers(numero);
+				// version map
+				// supprime marqueur
+			}
+
+		break;
+
+		default:
+			alert('error');
+
+	}
+
+
+
+
+	// Reset des label
+	lbl_choice = document.getElementsByClassName('lbl_choice');
+	input_choice = document.getElementsByClassName('input_choice');
+	valid_choice = document.getElementsByClassName('valid');
+	for(i = 1; i <= lbl_choice.length; i++)
+	{
+	   lbl_choice.item(i-1).innerHTML = "Choix "+i+'<span class="asterisc"> * </span>';
+	   lbl_choice.item(i-1).setAttribute('for', 'choix'+i);
+
+	   input_choice.item(i-1).setAttribute('id', 'choix'+i);
+	   //Affectation de la fonction à appeler 
+	   valid_choice.item(i-1).setAttribute("onclick","codeAddress(choix"+i+");");
+	}
+
+	if(document.getElementsByName("choices[]").length === 2)
+	{
+		document.getElementsByName("choices[]").css('cursor', 'no-drop');
+	}
+	else
+	{
+	   document.getElementsByName("choices[]").css('cursor', 'pointer');
+	}
+
+
+	initBlur();
+
+}
+
+/*
+ *Fonction de gestion de la connexion
+ *Lors de la création d'un sondage et que le créateur
+ *ne se soit pas connecté, ou n'ait pas de compte
+ */
+function manageConnectionForm(radio) {
+
+	switch(radio.id) {
+		
+		//Si possède déjà un compte
+		case 'registered':
+			$('.infos_user').css({display:'none'});
+			$('#pwd_user').css({display:'inline-block'});
+			document.getElementById('pwd_user').previousSibling.previousSibling.style.display = 'inline-block';
+		break;
+		
+		//Si première utilisation
+		case 'not_registered':
+			$('.infos_user').css({display:'inline-block'});
+			$('.info_box').css({display:'block'});
+			$("#pwd_user").css({display:'none'});
+			document.getElementById('pwd_user').previousSibling.previousSibling.style.display = 'none';
+		break;
+
+		default:
+			alert('Erreur');
+
+
+	}
+}
+
+function createAutocompletion(input){
+	autocomplete = new google.maps.places.Autocomplete(input, options);
+ 	
+  	google.maps.event.addListener(autocomplete, 'place_changed', function() {
+   	var place = autocomplete.getPlace();
+   	if (place.geometry.viewport) {
+    	geomap.fitBounds(place.geometry.viewport);
+   	} else {
+      	geomap.setCenter(place.geometry.location);
+      	geomap.setZoom(17);
+   	}
+	});
+}
+
+//fonction qui appelle l'ajout du marqueur en passant la recherche(autocomplétion) en paramètre et en gérant également le zoom
+function codeAddress(idAddress) {
+ 	var address = document.getElementById(idAddress.id).value;
+	// alert("valeur : "+address);
+   //var address = document.getElementById('address').value;
+  geocoder.geocode( { 'address': address}, function(resultat, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+     addMarker(resultat);
+     map.fitBounds(zonemarqueurs);
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
+// ajoute le marqueur
+function addMarker(resultat){	
+	for(var j = 0;j<markers.length;j++){
+		if(markers[j].position.A == resultat[0].geometry.location.A && markers[j].position.k == resultat[0].geometry.location.k )
+			boolMarker = true;
+	}
+	if(boolMarker == false){
+ 	 	var marker = new google.maps.Marker({
+    		position: resultat[0].geometry.location,
+    		map: map,
+    		draggable:true,
+    		animation: google.maps.Animation.DROP
+  		});
+  		markers.push(marker);
+  		posmarkers.push(marker.getPosition());
+  		zonemarqueurs.extend(marker.getPosition());  
+  	}
+  	boolMarker = false;
+}
+
+
+function deleteMarkers(numero){	  
+   
+    if(markers.length > 0 ){
+    	markers[numero-1].setMap(null);
+		markers.splice(numero-1,1);
+		posmarkers.splice(numero-1,1);
+		var zonemarqueurs = new google.maps.LatLngBounds();
+    	for (var i =0; i <markers.length;i++){
+      		zonemarqueurs.extend(markers[i].getPosition());
+    	}
+    	map.fitBounds(zonemarqueurs);
+    	if(markers.length == 0 ){
+    		markers = [];
+  			posmarkers = [];
+  			zonemarqueurs = new google.maps.LatLngBounds();
+    		map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    	}  
+    }
+}
