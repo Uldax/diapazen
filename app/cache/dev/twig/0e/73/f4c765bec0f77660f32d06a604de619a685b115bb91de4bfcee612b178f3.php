e6d3ca1f6d7d57e048d0b8d58860381135d9ab9a
Charly Bong <charles.bongiorno@isen.fr>
2014-05-07 11:12:11 +0200

    Début de l'intégration
    Fusion de symfony avec feature
    1 étape : type date , bug lors de l'ajout d'un input

--- app/cache/dev/twig/0e/73/f4c765bec0f77660f32d06a604de619a685b115bb91de4bfcee612b178f3.php
+++ app/cache/dev/twig/0e/73/f4c765bec0f77660f32d06a604de619a685b115bb91de4bfcee612b178f3.php
@@ -38,10 +38,11 @@ class __TwigTemplate_0e73f4c765bec0f77660f32d06a604de619a685b115bb91de4bfcee612b
         // line 12
         echo "\t\t<span class=\"grey_ariadne\" ><span></span><span>Partage</span><span></span></span>
 \t\t</div>
+
 \t\t
 \t\t<form onsubmit=\"return formCheck(this);\" id=\"poll_creation_form\" class=\"default_form\" action=\"connect\" method=\"post\">
 \t\t\t<h1 class=\"small_title\">Votre sondage de type ";
-        // line 16
+        // line 17
         echo twig_escape_filter($this->env, (isset($context["nametype"]) ? $context["nametype"] : $this->getContext($context, "nametype")), "html", null, true);
         echo "</h1>
 \t\t\t<label for=\"id_title_input\" class=\"text\">Titre<span class=\"asterisc\"> *</span></label>
@@ -51,7 +52,7 @@ class __TwigTemplate_0e73f4c765bec0f77660f32d06a604de619a685b115bb91de4bfcee612b
 \t\t\t<label for=\"id_description_input\" class=\"text lbl_textarea\">Description<span class=\"asterisc\"> *</span></label>
 \t\t\t<textarea class=\"small_text_edit\" id=\"id_description_input\" maxlength=\"1000\" placeholder=\"1000 carac. maximum.\" name=\"description_input\"></textarea>
 \t\t\t<h1 class=\"small_title\">Propositions de ";
-        // line 23
+        // line 24
         echo twig_escape_filter($this->env, (isset($context["nametype"]) ? $context["nametype"] : $this->getContext($context, "nametype")), "html", null, true);
         echo "</h1>
 \t\t\t<div id=\"choices\">
@@ -59,7 +60,7 @@ class __TwigTemplate_0e73f4c765bec0f77660f32d06a604de619a685b115bb91de4bfcee612b
 \t\t\t\t\t<label for=\"choix1\" class=\"text lbl_choice\">Choix 1<span class=\"asterisc\"> *</span></label>
 \t\t\t\t\t<input class=\"text_edit input_choice\" id=\"choix1\" type=\"text\" name=\"choices[]\" value=\"\" />
 \t\t\t\t\t<a class=\"grey_button\" title=\"Supprimer\" type=\"button\" onclick=\"manageChoices(this,'";
-        // line 28
+        // line 29
         echo twig_escape_filter($this->env, (isset($context["type"]) ? $context["type"] : $this->getContext($context, "type")), "html", null, true);
         echo "');\">x</a>
 \t\t\t\t</div>
@@ -67,7 +68,7 @@ class __TwigTemplate_0e73f4c765bec0f77660f32d06a604de619a685b115bb91de4bfcee612b
 \t\t\t\t\t<label for=\"choix2\" class=\"text lbl_choice\">Choix 2<span class=\"asterisc\"> *</span></label>
 \t\t\t\t\t<input class=\"text_edit input_choice\" id=\"choix2\" type=\"text\" name=\"choices[]\" value=\"\" />
 \t\t\t\t\t<a class=\"grey_button\" title=\"Supprimer\" type=\"button\" onclick=\"manageChoices(this,'";
-        // line 33
+        // line 34
         echo twig_escape_filter($this->env, (isset($context["type"]) ? $context["type"] : $this->getContext($context, "type")), "html", null, true);
         echo "');\">x</a>
 \t\t\t\t</div>
@@ -75,17 +76,17 @@ class __TwigTemplate_0e73f4c765bec0f77660f32d06a604de619a685b115bb91de4bfcee612b
 \t\t\t\t\t<label for=\"choix3\" class=\"text lbl_choice\">Choix 3<span class=\"asterisc\"> *</span></label>
 \t\t\t\t\t<input class=\"text_edit input_choice\" id=\"choix3\" type=\"text\" name=\"choices[]\" value=\"\" />
 \t\t\t\t\t<a class=\"grey_button\" title=\"Supprimer\" type=\"button\" onclick=\"manageChoices(this,'";
-        // line 38
+        // line 39
         echo twig_escape_filter($this->env, (isset($context["type"]) ? $context["type"] : $this->getContext($context, "type")), "html", null, true);
         echo "');\">x</a>
 \t\t\t\t</div>
 \t\t\t</div>
 \t\t\t<input class=\"orange_button\" type=\"submit\" value=\"Étape suivante\" />
 \t\t\t<a class=\"orange_button\" title=\"Ajouter un champ\" id=\"add_choice_button\" type=\"button\" onclick=\"manageChoices(this,'";
-        // line 42
+        // line 43
         echo twig_escape_filter($this->env, (isset($context["type"]) ? $context["type"] : $this->getContext($context, "type")), "html", null, true);
         echo "');\">+</a>
-\t\t</form>
+\t\t</form> 
 \t</div>
 
 \t<!--<script src=\"<?php \$this->getPath('js/script.js'); ?>\"> </script>
@@ -105,6 +106,6 @@ class __TwigTemplate_0e73f4c765bec0f77660f32d06a604de619a685b115bb91de4bfcee612b
 
     public function getDebugInfo()
     {
-        return array (  86 => 42,  79 => 38,  71 => 33,  63 => 28,  55 => 23,  45 => 16,  39 => 12,  36 => 9,  31 => 4,  28 => 3,);
+        return array (  76 => 69,  114 => 36,  100 => 65,  65 => 36,  167 => 59,  161 => 56,  153 => 53,  150 => 52,  126 => 38,  113 => 9,  104 => 7,  84 => 42,  77 => 38,  58 => 15,  34 => 5,  480 => 162,  474 => 161,  469 => 158,  461 => 155,  457 => 153,  453 => 151,  444 => 149,  440 => 148,  437 => 147,  435 => 146,  430 => 144,  427 => 143,  423 => 142,  413 => 134,  409 => 132,  407 => 131,  402 => 130,  398 => 129,  393 => 126,  387 => 122,  384 => 121,  381 => 120,  379 => 119,  374 => 116,  368 => 112,  365 => 111,  362 => 110,  360 => 109,  355 => 106,  341 => 105,  337 => 103,  322 => 101,  314 => 99,  312 => 98,  309 => 97,  305 => 95,  298 => 91,  294 => 90,  285 => 89,  283 => 88,  278 => 86,  268 => 85,  264 => 84,  258 => 81,  252 => 80,  247 => 78,  241 => 77,  229 => 73,  220 => 70,  214 => 69,  177 => 65,  169 => 60,  140 => 55,  132 => 40,  128 => 39,  107 => 36,  61 => 16,  273 => 96,  269 => 94,  254 => 92,  243 => 88,  240 => 86,  238 => 85,  235 => 74,  230 => 82,  227 => 81,  224 => 71,  221 => 77,  219 => 76,  217 => 75,  208 => 68,  204 => 72,  179 => 69,  159 => 61,  143 => 48,  135 => 53,  119 => 42,  102 => 32,  71 => 19,  67 => 65,  63 => 37,  59 => 36,  38 => 6,  94 => 28,  89 => 52,  85 => 25,  75 => 17,  68 => 14,  56 => 24,  87 => 43,  21 => 2,  26 => 6,  93 => 28,  88 => 43,  78 => 21,  46 => 17,  27 => 4,  44 => 16,  31 => 4,  28 => 3,  201 => 92,  196 => 90,  183 => 82,  171 => 60,  166 => 71,  163 => 62,  158 => 67,  156 => 66,  151 => 63,  142 => 46,  138 => 47,  136 => 41,  121 => 46,  117 => 10,  105 => 40,  91 => 63,  62 => 23,  49 => 19,  24 => 1,  25 => 3,  19 => 1,  79 => 42,  72 => 34,  69 => 25,  47 => 10,  40 => 7,  37 => 6,  22 => 1,  246 => 90,  157 => 55,  145 => 47,  139 => 45,  131 => 52,  123 => 38,  120 => 37,  115 => 43,  111 => 37,  108 => 36,  101 => 6,  98 => 67,  96 => 31,  83 => 25,  74 => 50,  66 => 29,  55 => 15,  52 => 21,  50 => 19,  43 => 8,  41 => 7,  35 => 5,  32 => 4,  29 => 3,  209 => 82,  203 => 78,  199 => 67,  193 => 73,  189 => 71,  187 => 84,  182 => 66,  176 => 64,  173 => 65,  168 => 72,  164 => 59,  162 => 57,  154 => 58,  149 => 51,  147 => 58,  144 => 49,  141 => 48,  133 => 45,  130 => 41,  125 => 44,  122 => 43,  116 => 41,  112 => 42,  109 => 8,  106 => 36,  103 => 32,  99 => 31,  95 => 5,  92 => 60,  86 => 51,  82 => 22,  80 => 39,  73 => 19,  64 => 29,  60 => 6,  57 => 11,  54 => 13,  51 => 12,  48 => 13,  45 => 17,  42 => 7,  39 => 12,  36 => 9,  33 => 4,  30 => 5,);
     }
 }
